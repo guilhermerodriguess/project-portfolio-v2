a8c97cc3eff8520832f4305ad5795648a1fca398
Guilherme <grsguiga@gmail.com>
2023-05-25 00:10:29 -0300

    fix: adds path /portfolio

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="App">
         <DefaultLayout >
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route exact path="/portfolio" element={<Home />} />
           </Routes>
         </DefaultLayout>
       </div>
